7bf7f7d5e04bd65e5b973cde6cb0d4d0b7223382
Dominic Evans <dominic.evans@uk.ibm.com>
2015-01-14 14:08:33 +0000

    python: switch to using $(PYTHON) from mqmake.tem* and add support for Windows

--- setup.py
+++ setup.py
@@ -18,10 +18,27 @@ IBM Corp.
 from setuptools import setup, find_packages, Extension
 from codecs import open as codecs_open
 from os import path
+from platform import system
 
 HERE = path.abspath(path.dirname(__file__))
 with codecs_open(path.join(HERE, 'description.rst'), encoding='utf-8') as f:
     LONG_DESCRIPTION = f.read()
+
+
+def get_sources():
+    """Return a list of source files to compile into the extension"""
+    if system() == 'Windows':
+        return [path.join('mqlight', 'cproton.cxx')]
+    else:
+        return [path.join('mqlight', 'cproton.c')]
+
+
+def get_runtime_library_dirs():
+    """Return a custom rpath to write into the extension"""
+    if system() == 'Linux':
+        return ['$ORIGIN']
+    else:
+        return []
 
 setup(
     name='mqlight',
@@ -46,14 +63,14 @@ setup(
     ],
     keywords='ibm mqlight',
     packages=find_packages(exclude=['tests']),
-    package_data={'mqlight': ['libqpid-proton*', 'samples/*.py']},
+    package_data={'mqlight': ['*.dll', 'libqpid-proton*', 'samples/*.py']},
     ext_modules=[
         Extension(name=path.join('mqlight', '_cproton'),
-                  sources=[path.join('mqlight', 'cproton.c')],
+                  sources=get_sources(),
                   include_dirs=[path.join(HERE, 'include')],
                   library_dirs=['mqlight'],
                   libraries=['qpid-proton'],
-                  runtime_library_dirs=['$ORIGIN']),
+                  runtime_library_dirs=get_runtime_library_dirs()),
     ],
     extras_require={
         'test': ['mock'],
